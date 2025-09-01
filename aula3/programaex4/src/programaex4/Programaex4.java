937b8ef241cdc9ea4e410e4a2e29f1bac2b5ba23
kauaapCS <kaua.pires@cs.cruzeirodosul.edu.br>
2025-09-01 19:54:53 -0300

    Update Programaex4.java

--- aula3/programaex4/src/programaex4/Programaex4.java
+++ aula3/programaex4/src/programaex4/Programaex4.java
@@ -3,22 +3,22 @@ import java.util.Scanner;
 
 public class Programaex4 {
     public static void main(String[] args) {
+        String sexo;
+        double altura, resultado = 0;
+        
         Scanner ler = new Scanner(System.in);
         
-        char sexo;
-        double altura, resultado;
-        
         System.out.println("Informe o sexo (M/F):");
-        sexo = ler.next().charAt(0);
+        sexo = ler.next().toLowerCase();
         
         System.out.println("Informe a altura em metros:");
         altura = ler.nextDouble();
         
-        if (sexo == "M".charAt(0)) {
+        if ("m".equals(sexo)) {
             resultado = (72.7 * altura) - 58;
         } else {
             resultado = (62.1 * altura) - 44.7;
-        }
+        };
         
         System.out.println("Peso ideal: " + resultado);
     }
